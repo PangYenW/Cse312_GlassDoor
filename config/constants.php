2478f44559be3fc54f12bf566b0b2ae05300c064
PangYenW <bwu477@gmail.com>
2019-08-15 00:24:59 -0400

    Update constants.php

--- config/constants.php
+++ config/constants.php
@@ -3,8 +3,8 @@
 define('PASSWORD', 'Ubstalkingcse442123456?');
 define('EMAIL', 'ubstalking@gmail.com');
 define('HOST', 'g3v9lgqa8h5nq05o.cbetxkdyhwsb.us-east-1.rds.amazonaws.com');
-define('USER', 'ck73gz3ovh15e2d4');
-define('PASS', 'nipet0b5dvchj94v');
-define('DB', 'v4oyfnhiyhaeaikb');
+define('USER', 'ocm8mmcw7wnl4bl2');
+define('PASS', 'lr2274chrnlqaxm0');
+define('DB', 'igum6866sbko3bjo');
 define('ADMIN', 'admin@project.ed')
  ?>
